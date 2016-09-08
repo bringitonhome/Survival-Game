bbdedfe25e764fe081166715aaa56d527c15fda4
Joel Therrien Vrignon <joel_t.v@hotmail.com>
2016-09-07 21:36:39 -0500

    September 7th, 2016
    -Added bullet impact (no effect yet)
    -Moved setSpriteSheet() to a function usable by all classes
    -Added character screen side-to-side teleportation
    -Added Gore class, Blood & Chunks subclasses
    -Added framerate limit, changed speeds appropriately
    -Put Enemies in list instead of array, allows for more enemies

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 #include <ctime>
 #include <list>
 
-#define WW 800
-#define WH 600
-#define SCALE 5
-#define BASESPEED 0.1
+#define WW 1920
+#define WH 1080
+#define SCALE 4
+#define BASESPEED 5
 
 #define DETECTRANGE 200
 #define FIRERATE 200
@@ -20,18 +20,18 @@
 
 using namespace std;
 
+
+static sf::Texture spriteSheetTxt;
+
+void setSprite(sf::Sprite* sprite, int spriteOriginX, int spriteOriginY, int spriteWidth, int spriteHeight){
+    (*sprite).setTexture(spriteSheetTxt);
+    (*sprite).setTextureRect(sf::IntRect(spriteOriginX, spriteOriginY, spriteWidth, spriteHeight));
+    (*sprite).setScale(SCALE, SCALE);
+}
+
 // Base class
 class Character{
 public:
-
-    Character();
-
-    void setSprite(sf::Texture* texture){
-        sprite.setTexture(*texture);
-        sprite.setTextureRect(sf::IntRect(spriteX, spriteY, spriteWidth, spriteHeight));
-        sprite.setScale(SCALE, SCALE);
-        sprite.setPosition(sf::Vector2f(xPos, yPos));
-    }
 
     sf::Sprite getSprite(){
         return sprite;
@@ -48,8 +48,8 @@ public:
 
 protected:
     sf::Sprite sprite;
-    int spriteX;
-    int spriteY;
+    int spriteOriginX;
+    int spriteOriginY;
     int spriteWidth;
     int spriteHeight;
 
@@ -59,12 +59,6 @@ protected:
 
     float speed;
 };
-
-Character::Character(){
-    spriteX = 4;
-    spriteHeight = 11;
-    sprite.setOrigin(5.5, 5.5);
-}
 
 class Hero : public Character{
 public:
@@ -101,8 +95,8 @@ public:
         setSpeed();
 
         if(moving == true){
-            xPos += speed*cos(moveAngle);
-            yPos += speed*sin(moveAngle);
+            xPos += speed*cos(moveAngle) + (xPos < -11*SCALE)*(WW+22*SCALE) - (xPos > WW + 11*SCALE)*(WW+22*SCALE);
+            yPos += speed*sin(moveAngle) + (yPos < -11*SCALE)*(WH+22*SCALE) - (yPos > WH + 11*SCALE)*(WH+22*SCALE);
         }
 
         lookAngle = atan2(sf::Mouse::getPosition(*window).y - yPos, sf::Mouse::getPosition(*window).x - xPos);
@@ -128,13 +122,18 @@ private:
 };
 
 Hero::Hero(){
-    spriteY = 4;
+    spriteOriginX = 5;
+    spriteOriginY = 5;
     spriteWidth = 13;
+    spriteHeight = 11;
+    sprite.setOrigin(5.5, 5.5);
     xPos = WW/4;
     yPos = WH/2;
     speed = BASESPEED;
 
     grabbed = false;
+
+    setSprite(&sprite, spriteOriginX, spriteOriginY, spriteWidth, spriteHeight);
 }
 
 class Enemy : public Character{
@@ -150,18 +149,10 @@ public:
         chasing = false;
         wandering = true;
 
-        bool newDirection = (rand()%100000 <= 10);
+        bool newDirection = (rand()%1000 <= 10);
         if(newDirection == true){
             lookAngle = (rand()%360)*M_PI/180;
         }
-
-        setSpeed();
-
-        xPos += cos(lookAngle)*speed;
-        yPos += sin(lookAngle)*speed;
-
-        sprite.setRotation(lookAngle*180/M_PI);
-        sprite.setPosition(xPos, yPos);
     }
 
     void chaseHero(float heroPosX, float heroPosY, float heroDistance){
@@ -169,20 +160,8 @@ public:
         wandering = false;
         chasing = true;
 
-        setSpeed();
-
         lookAngle = atan2(heroPosY - yPos, heroPosX - xPos);
-        if(heroDistance > 50){
-            xPos += cos(lookAngle)*speed;
-            yPos += sin(lookAngle)*speed;
-            sprite.setRotation(lookAngle*180/M_PI);
-            sprite.setPosition(xPos, yPos);
-        }
-        else if(heroDistance <= 50){
-            xPos -= cos(lookAngle)*speed;
-            yPos -= sin(lookAngle)*speed;
-            sprite.setPosition(xPos, yPos);
-        }
+
         if(heroDistance > 75){
             grabbing = false;
         }
@@ -199,12 +178,20 @@ public:
 
         if(heroDistance <= DETECTRANGE){
             chaseHero(heroPosX, heroPosY, heroDistance);
+            (*hero).setGrabbed(grabbing);
         }
         else if(heroDistance > DETECTRANGE){
             wander();
         }
 
-        (*hero).setGrabbed(grabbing);
+        setSpeed();
+
+        xPos += cos(lookAngle)*(1 - 2*(heroDistance < 50))*speed + (xPos < -6*SCALE)*(WW+12*SCALE) - (xPos > WW+6*SCALE)*(WW+12*SCALE);
+        yPos += sin(lookAngle)*(1 - 2*(heroDistance < 50))*speed + (yPos < -6*SCALE)*(WH+12*SCALE) - (yPos > WH+6*SCALE)*(WH+12*SCALE);
+
+        sprite.setRotation(lookAngle*180/M_PI);
+        sprite.setPosition(xPos, yPos);
+
     }
 
 
@@ -219,25 +206,28 @@ private:
 };
 
 Enemy::Enemy(){
-    spriteY = 4 + spriteHeight;
+    spriteOriginX = 5;
+    spriteOriginY = 20;
     spriteWidth = 11;
+    spriteHeight = 11;
+    sprite.setOrigin(5.5, 5.5);
+    setSprite(&sprite, spriteOriginX, spriteOriginY, spriteWidth, spriteHeight);
+
     xPos = rand()%WW;
     yPos = rand()%WH;
+
     chasing = false;
     grabbing = false;
     wounded = false;
+
+
 }
+
 class Bullet{
 public:
 
     Bullet(Hero* hero);
 
-    void setSprite(sf::Texture* texture){
-        sprite.setTexture(*texture);
-        sprite.setTextureRect(sf::IntRect(5, 27, 2, 1));
-        sprite.setScale(SCALE, SCALE);
-        sprite.setRotation(direction*180/M_PI);
-    }
     sf::Sprite getSprite(){
         return sprite;
     }
@@ -247,6 +237,22 @@ public:
 
         sprite.setPosition(xPos, yPos);
     }
+
+    void setBulletSpeed(){
+        bulletSpeed = 2*BASESPEED;
+    }
+
+    bool checkForImpact(list<Enemy*> enemy_list){
+        //impact = (*enemy).getSprite().getGlobalBounds().intersects(sprite.getGlobalBounds());
+        for(list<Enemy*>::iterator it = enemy_list.begin(); it != enemy_list.end(); ++it){
+            impact = (sprite.getGlobalBounds().intersects((*(*it)).getSprite().getGlobalBounds()));
+            if(impact == true){
+                return impact;
+            }
+        }
+        return impact;
+    }
+
     float getPositionX(){
         return xPos;
     }
@@ -254,56 +260,126 @@ public:
         return yPos;
     }
 
+    float getImpactAngle(){
+        return direction;
+    }
+
+    float getBulletSpeed(){
+        return bulletSpeed;
+    }
+
 
 private:
     sf::Sprite sprite;
+
+    bool impact;
+
+    int spriteOriginX;
+    int spriteOriginY;
+    int spriteWidth;
+    int spriteHeight;
+
     float xPos;
     float yPos;
     float direction;
+    float bulletSpeed;
 };
 
 Bullet::Bullet(Hero* hero){
+
     xPos = (*hero).getPositionX();
     yPos = (*hero).getPositionY();
     direction = (*hero).getLookAngle();
+    sprite.setRotation(direction*180/M_PI);
+
+    impact = false;
+
+    spriteOriginX = 5;
+    spriteOriginY = 35;
+    spriteWidth = 2;
+    spriteHeight = 1;
+
+    setSprite(&sprite, spriteOriginX, spriteOriginY, spriteWidth, spriteHeight);
+
+
+    setBulletSpeed();
+
 }
+
+class Gore{
+public:
+    Gore(Bullet* bullet);
+
+    sf::Sprite getSprite(){
+        return sprite;
+    }
+
+private:
+    float spawnX;
+    float spawnY;
+    float impactAngle;
+    float goreSpeed;
+
+    sf::Sprite sprite;
+};
+
+Gore::Gore(Bullet* bullet){
+    spawnX = (*bullet).getPositionX();
+    spawnY = (*bullet).getPositionY();
+    impactAngle = (*bullet).getImpactAngle();
+    goreSpeed = (*bullet).getBulletSpeed();
+}
+
+class Blood : public Gore{
+public:
+    Blood(Bullet* bullet) : Gore::Gore(bullet){
+    }
+
+private:
+
+};
+
+class Chunks : public Gore{
+public:
+    Chunks(Bullet* bullet) : Gore::Gore(bullet){
+
+    }
+
+private:
+
+};
+
 
 int main()
 {
     srand(time(0));
     sf::RenderWindow window(sf::VideoMode(WW, WH), "Survival Game 1993");
+    window.setFramerateLimit(60);
 
     sf::Image spriteSheetImg;
     if(!spriteSheetImg.loadFromFile("MasterSpriteSheet.png")){
         cout << "Failed to load image" << endl;
     }
     spriteSheetImg.createMaskFromColor(sf::Color::Magenta);
-    sf::Texture spriteSheetTxt;
+
     if(!spriteSheetTxt.loadFromImage(spriteSheetImg)){
         cout << "Failed to load texture" << endl;
     }
 
-    sf::Sprite roadSprite[2];
-    for(int road = 0; road < 2; road++){
-        roadSprite[road].setTexture(spriteSheetTxt);
-        roadSprite[road].setTextureRect(sf::IntRect(4, 49, 155, 51));
-        roadSprite[road].setScale(SCALE, SCALE);
-        roadSprite[road].setPosition(-45*SCALE + 100*SCALE*road, WH/3);
-    }
-
-
     Hero hero;
-    int numEnemies = 1 + rand()%10;
-    Enemy enemy[numEnemies];
 
     list<Bullet*> bullet_list;
+    list<Enemy*> enemy_list;
+
     time_t currentTime;
     time_t timeOfPreviousBullet = 0;
 
-    hero.setSprite(&spriteSheetTxt);
-    for(int x = 0; x < numEnemies; x++){
-        enemy[x].setSprite(&spriteSheetTxt);
+    int numEnemies = 100;
+
+    for(int enemy = 0; enemy < numEnemies; enemy++){
+        enemy_list.push_back(new Enemy);
     }
+
 
     while(window.isOpen()){
         sf::Event event;
@@ -314,35 +390,36 @@ int main()
             }
         }
 
+        window.clear(sf::Color::Green);
+
         if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){;
             currentTime = clock();
             time_t timeSinceLastBullet = currentTime - timeOfPreviousBullet;
             if(timeSinceLastBullet > FIRERATE){
                 bullet_list.push_back(new Bullet(&hero));
-                (*(bullet_list.back())).setSprite(&spriteSheetTxt);
                 timeOfPreviousBullet = currentTime;
             }
         }
 
-        window.clear(sf::Color::Green);
+
 
         hero.moveChar(&window);
-        for(int x = 0; x < numEnemies; x++){
-            enemy[x].moveZombie(&hero);
+
+        for(list<Enemy*>::iterator it=enemy_list.begin(); it != enemy_list.end(); ++it){
+            (*(*it)).moveZombie(&hero);
         }
 
-        for(int road = 0; road < 2; road++){
-            window.draw(roadSprite[road]);
-        }
 
 
 
         for(list<Bullet*>::iterator it=bullet_list.begin(); it != bullet_list.end(); ++it){
+            bool impact = false;
 
-            if((*(*it)).getPositionX() < 0 || (*(*it)).getPositionX() > WW || (*(*it)).getPositionY() < 0 || (*(*it)).getPositionY() > WH){
+            impact = (*(*it)).checkForImpact(enemy_list);
+
+            if((*(*it)).getPositionX() < -100 || (*(*it)).getPositionX() > WW + 100 || (*(*it)).getPositionY() < -100 || (*(*it)).getPositionY() > WH + 100 || impact == true){
 
                 it = bullet_list.erase(it);
-                cout << "Bullet Erased" << endl;
             }
             else{
                 (*(*it)).moveBullet();
@@ -352,8 +429,8 @@ int main()
 
         window.draw(hero.getSprite());
 
-        for(int x = 0; x < numEnemies; x++){
-            window.draw(enemy[x].getSprite());
+        for(list<Enemy*>::iterator it=enemy_list.begin(); it != enemy_list.end(); ++it){
+            window.draw((*(*it)).getSprite());
         }
 
 
